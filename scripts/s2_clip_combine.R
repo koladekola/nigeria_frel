5b76ba03f796e8c676a2290f26a153e3d8b5fe37
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2018-12-07 09:27:21 +0000

    standardize the map with REF codes

--- scripts/s2_clip_combine.R
+++ scripts/s2_clip_combine.R
@@ -112,7 +112,7 @@ system(sprintf("gdal_calc.py -A %s -B %s --co COMPRESS=LZW --outfile=%s --calc=\
                paste0("(A>0)*(A-B)+(A==0)*(B==1)*0")
 ))
 
-#################### COMBINATION INTO DD MAP (1==NF, 2==F, 3==Df, 4==Dg, 5==ToF, 6==Dg_TOF)
+#################### COMBINATION INTO DD MAP (1==Forest, 2==NonForest, 3==gain, 4==deforestation, 5==Degradation 6==ToF, 7==Dg_TOF)
 system(sprintf("gdal_calc.py -A %s -B %s -C %s -D %s -E %s -F %s -G %s --co COMPRESS=LZW --outfile=%s --calc=\"%s\"",
                paste0(dd_dir,"tmp_gfc_tc_start.tif"),
                paste0(dd_dir,"tmp_gfc_loss_sieve.tif"),
@@ -122,10 +122,10 @@ system(sprintf("gdal_calc.py -A %s -B %s -C %s -D %s -E %s -F %s -G %s --co COMP
                paste0(dd_dir,"tmp_gfc_tc_start_fsieve.tif"),
                paste0(dd_dir,"tmp_gfc_tc_start_inf.tif"),
                paste0(dd_dir,"tmp_dd_map.tif"),
-               paste0("(A==0)*1+",
-                      "(A>0)*((B==0)*(C==0)*((D>0)*2+(E>0)*5)+",
-                             "(B>0)*3+",
-                             "(C>0)*((F>0)*4+(G>0)*6))")
+               paste0("(A==0)*2+",
+                      "(A>0)*((B==0)*(C==0)*((D>0)*1+(E>0)*6)+",
+                             "(B>0)*4+",
+                             "(C>0)*((F>0)*5+(G>0)*7))")
 ))
 
 #############################################################
@@ -139,7 +139,7 @@ system(sprintf("python %s/oft-cutline_crop.py -v %s -i %s -o %s -a %s",
 ))
 
 #################### CREATE A COLOR TABLE FOR THE OUTPUT MAP
-my_classes <- c(0,1,2,3,4,5,6)
+my_classes <- c(0,2,1,4,5,6,7)
 my_colors  <- col2rgb(c("black","grey","darkgreen","red","orange","lightgreen","yellow"))
 
 pct <- data.frame(cbind(my_classes,
